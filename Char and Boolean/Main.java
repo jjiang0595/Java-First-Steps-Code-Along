eb6d9e63a58783dd489008819f161a16449f25f0
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2020-07-14 15:21:43 -0400

    Create Main.java

---
+++ Char and Boolean/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char mySecondUnicodeChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(mySecondUnicodeChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
