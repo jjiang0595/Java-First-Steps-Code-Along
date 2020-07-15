34dd3d823b37762dd727ca30ce4eb3921708e94b
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2020-07-15 15:57:00 -0400

    Create Main.java

---
+++ Keywords and Expressions/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double kilometres = (100 * 1.609);
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
